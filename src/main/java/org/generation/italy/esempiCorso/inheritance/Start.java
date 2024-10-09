83798ed304b3f32488fd48e72d1861c5f820a180
Pamela Milanese <milanesepamela@gmail.com>
2024-10-09 12:56:03 +0200

    Prova

--- src/main/java/org/generation/italy/esempiCorso/inheritance/Start.java
+++ src/main/java/org/generation/italy/esempiCorso/inheritance/Start.java
@@ -4,6 +4,6 @@ public class Start {
     public static void main(String[] args){
         System.out.println("testing github");
         System.out.println("Aggiunto da Alessia");
-        System.out.println("ciao");
+        System.out.println("Ciao");
     }
 }
