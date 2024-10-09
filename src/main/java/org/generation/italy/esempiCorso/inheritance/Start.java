435d61ffcc97e113f80515d5804382b6929ba741
Danilo Vulpiani <danilovulpiani@libero.it>
2024-10-09 12:44:44 +0200

    mostra uso delle branch

--- src/main/java/org/generation/italy/esempiCorso/inheritance/Start.java
+++ src/main/java/org/generation/italy/esempiCorso/inheritance/Start.java
@@ -4,5 +4,6 @@ public class Start {
     public static void main(String[] args){
         System.out.println("testing github");
         System.out.println("Aggiunto da Alessia");
+        System.out.println("branch di Danilo Vulpiani");
     }
 }
