b982e46aa2fa8e1dcab68f9dec2a4e116f1bdc7a
GianlucaPricopie <gianlucapricopie5@gmail.com>
2024-10-09 13:51:17 +0200

    prova branch

--- src/main/java/org/generation/italy/esempiCorso/inheritance/Start.java
+++ src/main/java/org/generation/italy/esempiCorso/inheritance/Start.java
@@ -4,7 +4,7 @@ public class Start {
     public static void main(String[] args){
         System.out.println("testing github");
         System.out.println("Aggiunto da Alessia");
-        System.out.println("Prova branch");
+        System.out.println("Prova branc");
     }
 
 }
