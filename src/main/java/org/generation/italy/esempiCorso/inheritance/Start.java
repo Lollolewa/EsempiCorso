69e3535f7bcfcbd9fb4187eb93921ab9140566c8
Pietro Marinari <pietro.marinari@gmail.com>
2024-10-09 12:49:47 +0200

    branch commit test

--- src/main/java/org/generation/italy/esempiCorso/inheritance/Start.java
+++ src/main/java/org/generation/italy/esempiCorso/inheritance/Start.java
@@ -1,4 +1,4 @@
-package org.generation.italy.esempiCorso.inheritance;
+package org.generation.italy.esempiCorso.inheritance; // bestBranch
 
 public class Start {
     public static void main(String[] args){
