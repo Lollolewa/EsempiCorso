b374fb132ef53e6d959bb84727e82a60b2e5c889
Alessia Pallavera <alessia.pallavera01@gmail.com>
2024-10-09 12:33:04 +0200

    fa una modifica per fare pratica con git/github

--- src/main/java/org/generation/italy/esempiCorso/inheritance/Start.java
+++ src/main/java/org/generation/italy/esempiCorso/inheritance/Start.java
@@ -3,5 +3,6 @@ package org.generation.italy.esempiCorso.inheritance;
 public class Start {
     public static void main(String[] args){
         System.out.println("testing github");
+        System.out.println("Aggiunto da Alessia");
     }
 }
