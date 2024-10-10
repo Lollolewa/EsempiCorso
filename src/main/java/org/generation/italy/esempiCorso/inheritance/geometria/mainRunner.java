709f8316b1beb78a3d6e6b944b3622f60c989d1c
Mirko Risuleo <mirkotm705@gmail.com>
2024-10-10 15:03:06 +0200

    Aggiornato con esercizio geometria

---
+++ src/main/java/org/generation/italy/esempiCorso/inheritance/geometria/mainRunner.java
@@ -0,0 +1,13 @@
+package org.generation.italy.esempiCorso.inheritance.geometria;
+
+public class mainRunner {
+    public static void main(String[] args) {
+
+        Triangolo triangle = new Triangolo(3,2);
+        Rettangolo rectangle = new Rettangolo(2, 10);
+
+        System.out.println(triangle.calcolaArea());
+        System.out.println(rectangle.calcolaArea());
+
+    }
+}
