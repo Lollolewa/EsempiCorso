3f8db6fd1b69cb55da3a40858b987b004848586a
Joshua Castellani <joshuacastellani@gmail.com>
2024-10-10 12:37:11 +0200

    ho aggiunto la calsse forma e le due forme

--- src/main/java/org/generation/italy/esempiCorso/inheritance/Start.java
+++ src/main/java/org/generation/italy/esempiCorso/inheritance/Start.java
@@ -1,11 +1,16 @@
 package org.generation.italy.esempiCorso.inheritance;
 
+import org.generation.italy.esempiCorso.inheritance.geometria.Rettangolo;
+import org.generation.italy.esempiCorso.inheritance.geometria.Triangolo;
 import org.generation.italy.esempiCorso.inheritance.school.Teacher;
 
 public class Start {
     public static void main(String[] args){
        Teacher teacher = new Teacher("Riccardo","Audano",18, "programmazione", 18);
        System.out.println(teacher.toString()); //tutti gli oggetti creati derivano dalla super classe object
-
+        Triangolo triangolo = new Triangolo(10, 10);
+        Rettangolo rettangolo = new Rettangolo(5,10);
+        System.out.println(triangolo.calcolaArea());
+        System.out.println(rettangolo.calcolaArea());
     }
 }
---
+++ src/main/java/org/generation/italy/esempiCorso/inheritance/geometria/Triangolo.java
@@ -0,0 +1,11 @@
+package org.generation.italy.esempiCorso.inheritance.geometria;
+
+public class Triangolo extends Forma {
+    public Triangolo(int base, int altezza) {
+        super(base, altezza);
+    }
+    @Override
+    public int calcolaArea() {
+        return (this.base * this.altezza) / 2;
+    }
+}
