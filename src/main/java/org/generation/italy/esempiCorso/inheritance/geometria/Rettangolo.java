1dee506d1858e8c74e55f608eb9270b367aa9d7f
GianlucaPricopie <gianlucapricopie5@gmail.com>
2024-10-10 12:50:54 +0200

    commit sulla creazione della classe forma che si estende in rettangolo e triangolo

--- src/main/java/org/generation/italy/esempiCorso/inheritance/geometria/Rettangolo.java
+++ src/main/java/org/generation/italy/esempiCorso/inheritance/geometria/Rettangolo.java
@@ -1,7 +1,8 @@
 package org.generation.italy.esempiCorso.inheritance.geometria;
 
 public class Rettangolo extends Forma{
-
+//se non invochiamo il costruttore della classe esplicitamente il compilatore inserisce
+    //un invocazione al costruttore di default della classe madre
     public Rettangolo(int altezza ,int base) {
         super( altezza, base);
         this.base = base;
