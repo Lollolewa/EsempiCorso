178e928fdbafae8d35708f46c65bdc2b3c8fed22
GianlucaPricopie <gianlucapricopie5@gmail.com>
2024-10-10 12:35:12 +0200

    commit sulla creazione della classe forma che si estende in rettangolo e triangolo

--- src/main/java/org/generation/italy/esempiCorso/inheritance/Start.java
+++ src/main/java/org/generation/italy/esempiCorso/inheritance/Start.java
@@ -1,9 +1,5 @@
 package org.generation.italy.esempiCorso.inheritance;
 
-import org.generation.italy.esempiCorso.inheritance.geometria.Forma;
-import org.generation.italy.esempiCorso.inheritance.geometria.Rettangolo;
-import org.generation.italy.esempiCorso.inheritance.geometria.Triangolo;
-
 public class Start {
     public static void main(String[] args){
 
