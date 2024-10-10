467df9efaa403ba1b2599068ca8032ad5d9472ab
Pietro Marinari <pietro.marinari@gmail.com>
2024-10-10 12:21:18 +0200

    branch commit test

--- src/main/java/org/generation/italy/esempiCorso/inheritance/Start.java
+++ src/main/java/org/generation/italy/esempiCorso/inheritance/Start.java
@@ -1,11 +1,14 @@
 package org.generation.italy.esempiCorso.inheritance; // bestBranch
 
 import org.generation.italy.esempiCorso.inheritance.school.Teacher;
+import org.generation.italy.esempiCorso.inheritance.geometria.*;
 
 public class Start {
     public static void main(String[] args){
        Teacher teacher = new Teacher("Riccardo","Audano",18, "programmazione", 18);
        System.out.println(teacher.toString()); //tutti gli oggetti creati derivano dalla super classe object
+
+        System.out.println();
 
     }
 }
---
+++ src/main/java/org/generation/italy/esempiCorso/inheritance/geometria/Forma.java
@@ -0,0 +1,13 @@
+package org.generation.italy.esempiCorso.inheritance.geometria;
+
+public class Forma {
+    protected int base, altezza;
+
+    public Forma (int base, int altezza){
+        this.base = base;
+        this.altezza = altezza;
+    }
+    public int calcolaArea(){
+        return 0;
+    }
+}
