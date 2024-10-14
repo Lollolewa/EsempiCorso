e9311e9d48a60aa1da6ea6b424f5b6efcf432414
Pamela Milanese <milanesepamela@gmail.com>
2024-10-14 12:59:13 +0200

    Altra commit

--- src/main/java/org/generation/italy/esempiCorso/interfaces/Start.java
+++ src/main/java/org/generation/italy/esempiCorso/interfaces/Start.java
@@ -6,5 +6,17 @@ public class Start {
     public static void main(String[] args) {
         //.StringUtilities st = new StringUtilities();
         String result = StringUtilities.invert("ciao");
+        HumanBeing hb = new Accountant();
+        hb.speak();
+        Accountant ac = new Accountant();
+        inviteForDinner(ac);
+        SuperHero sh = new SuperHero();
+        inviteForDinner(sh);
+
+    }
+    public static void inviteForDinner(HumanBeing h){
+        h.speak();
+        h.eat();
+
     }
 }
