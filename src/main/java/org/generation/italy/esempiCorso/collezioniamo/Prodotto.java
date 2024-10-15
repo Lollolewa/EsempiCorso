1953445682bade5ccd5ed6a645ee91a5aad221ce
Ejran Mustafoski <caschiamo1234@gmail.com>
2024-10-15 17:44:15 +0200

    Ereditarietà

---
+++ src/main/java/org/generation/italy/esempiCorso/collezioniamo/Prodotto.java
@@ -0,0 +1,39 @@
+package org.generation.italy.esempiCorso.collezioniamo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Prodotto {
+    String nome;
+    int costo;
+
+    public Prodotto(String nome, int costo) {
+        this.nome = nome;
+        this.costo = costo;
+
+
+    }
+    @Override
+    public boolean equals(Object o){
+        System.out.println("Ho chiamato equals");
+        if(o == null || o.getClass() != Prodotto.class){
+            return false;
+       }
+        Prodotto other = (Prodotto) o;
+        if(this.nome == null ){
+            if( other.nome == null){
+                   return costo == other.costo;
+            }
+           return false;
+        }
+       return this.nome.equals(other.nome) && (this.costo == other.costo);
+    }
+    public String aCaso(){
+        return"ciao";
+    }
+    @Override
+    public int hashCode(){
+        System.out.println("Ho chiamato hasCode");
+        return(nome != null ? nome.hashCode() : 1 ) + costo;
+    }
+}
