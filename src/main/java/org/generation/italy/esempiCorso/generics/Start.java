304329172b3ddd2c797678676f9f84b69e958295
Mirko Risuleo <mirkotm705@gmail.com>
2024-10-16 12:58:55 +0200

    Aggiornamenti vari a lezione

---
+++ src/main/java/org/generation/italy/esempiCorso/generics/Start.java
@@ -0,0 +1,9 @@
+package org.generation.italy.esempiCorso.generics;
+
+public class Start {
+    public static void main(String[] args) {
+        //generics non esistevano in principio
+        //classi generics la maggior parte delle volte sono strutture dati.
+
+    }
+}
