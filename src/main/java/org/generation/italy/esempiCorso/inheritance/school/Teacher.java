1a1747de62d69da71757c78b261f6590384f2e57
Lorenzo Masci <lorenzozero5@gmail.com>
2024-10-17 12:54:01 +0200

    Commit di backup con aereo, appartamento, archivio e scuola

--- src/main/java/org/generation/italy/esempiCorso/inheritance/school/Teacher.java
+++ src/main/java/org/generation/italy/esempiCorso/inheritance/school/Teacher.java
@@ -1,24 +1,21 @@
 package org.generation.italy.esempiCorso.inheritance.school;
-//ereditarietà e' il rapporto piu' stretto tra classi (is_a è la domanda per capire se ha senso l'ereditarieta')
-public class Teacher extends Person{
-    //teacher è una sottoclasse/sottotipo di person (supertipo/superclasse)
-    //la sottoclasse eredita le proprietà e i metodi della superclasse ma non accede direttamente alle variabili private
-    //keyword che mantiene la variabile privata ma la rende visibile alle sottoclassi
+public class Teacher extends Employee{
     private String subject;
     private int yearOfService;
 
-    public Teacher(String name, String surname, int age, String subject, int yearOfService){
-        //this non funziona, si utilizza un altra keyword -> super(una superclasse per ogni classe)
-        super(name, surname, age); //richiami il costruttore della superclasse, ho creato una person
-        this.subject=subject;
-        this.yearOfService=yearOfService;
+    public Teacher(String name, String surname, int age, String subject, int yearOfService, int monthSalary, int nMonths){
+        super(name, surname, age, monthSalary, nMonths);
+        this.subject = subject;
+        this.yearOfService = yearOfService;
     }
 
 
-    @Override //riprende il metodo della superclasse e lo sovrascrive, non necessario
-    //non commento ma @ per obbligare il compilatore a verificare se esiste realmente la funzione nella superclasse
-    //in altri linguaggi la keyword override si scrive nel metodo dopo public
+    @Override
     public String toString(){
-        return super.toString() + " Insegno " + subject + " da " + yearOfService + " anni.";
+        return super.toString() + "\n" + " Insegno " + subject + " da " + yearOfService + " anni.";
+    }
+
+    public int getAnnualSalary(){
+        return yearOfService > 10 ? super.getAnnualSalary() + 500 : super.getAnnualSalary();
     }
 }
