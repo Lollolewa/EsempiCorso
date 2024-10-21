c0f21392c05ca6461e12ff7316735bf1e76d21ea
Andrea Macellaro La Franca <andrea.macellaro@gmail.com>
2024-10-21 16:16:05 +0200

    Agenzia viaggio RocketClaw

--- src/main/java/org/generation/italy/esempiCorso/inheritance/examples/interfaces/HumanBeing.java
+++
@@ -1,8 +0,0 @@
-package org.generation.italy.esempiCorso.inheritance.examples.interfaces;
-
-public interface HumanBeing {
-    int maxAge = 120; //ciò che capisce il compilatore è " public static final int max age", ovvero questo valore sarà static e una costante;
-    void speak(); //tutti i metodi di un interfaccia sono public e devono essere abstract; quindi non facciamo "public abstract void speak" perchè sarebbe ridondante, è cosi e basta.
-    void eat();
-    void work(int hours); //possiamo metterci tutti i metodi astratti che vogliamo;
-}
--- src/main/java/org/generation/italy/esempiCorso/utilities/StringUtilities.java
+++
@@ -1,8 +0,0 @@
-package org.generation.italy.esempiCorso.utilities;
-
-public class StringUtilities {
-    public static String invert(String target){
-        return null;
-        //.
-    }
-}
