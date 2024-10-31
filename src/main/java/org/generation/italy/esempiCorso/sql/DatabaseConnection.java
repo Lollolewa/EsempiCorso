bb9834d16f9a30269ce3cf7bea077544a406bc51
Mirko Risuleo <mirkotm705@gmail.com>
2024-10-31 16:44:49 +0100

    Commit con lezione su database

--- src/main/java/org/generation/italy/esempiCorso/sql/DatabaseConnection.java
+++ src/main/java/org/generation/italy/esempiCorso/sql/DatabaseConnection.java
@@ -3,28 +3,71 @@ package org.generation.italy.esempiCorso.sql;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Optional;
 
 public class DatabaseConnection {
 
-    public static Connection getConnection() {
-        String jdbcUrl = "jdbc:postgresql://localhost:5432/library"; //versione più bruttina
-        //jdbc = Java Databasse Connectivity
+    public static Optional<Connection> getConnection(){
+        String jdbcUrl = "jdbc:postgresql://localhost:5432/library"; //la versione più bruttina
         String username = "postgresMaster";
         String password = "goPostgresGo";
 
-        Connection connection = null;
-        //Connection è un oggetto, da immaginare come un tubo che collega il nostro codice al database. Manderemo da java degli oggetti, che trasportano
-        //le query, e il database ci restituirà altri oggetti che contengono le informazioni richieste nelle query.
-
-        try {
-            connection = DriverManager.getConnection(jdbcUrl, username, password); //il metodo getConnection può ritornare un'eccezione
-
+        try{
+            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+            return Optional.of(connection);
         }
-        catch(SQLException e) {
+        catch(SQLException e){
             e.printStackTrace();
-            System.out.println("Non mi posso collegare al DB");
+            System.out.println("Non mi posso collegare al db :("); //per db si intende database
+            return Optional.empty();
         }
-
-        return connection;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    public static Connection getConnection() {
+//        String jdbcUrl = "jdbc:postgresql://localhost:5432/library"; //versione più bruttina
+//        //jdbc = Java Databasse Connectivity
+//        String username = "postgresMaster";
+//        String password = "goPostgresGo";
+//
+//        Connection connection = null;
+//        //Connection è un oggetto, da immaginare come un tubo che collega il nostro codice al database. Manderemo da java degli oggetti, che trasportano
+//        //le query, e il database ci restituirà altri oggetti che contengono le informazioni richieste nelle query.
+//
+//        try {
+//            connection = DriverManager.getConnection(jdbcUrl, username, password); //il metodo getConnection può ritornare un'eccezione
+//
+//        }
+//        catch(SQLException e) {
+//            e.printStackTrace();
+//            System.out.println("Non mi posso collegare al DB");
+//        }
+//
+//        return connection;
+//    }
+//}
