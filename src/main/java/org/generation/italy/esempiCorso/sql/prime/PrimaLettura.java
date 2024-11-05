b5b31b18ba103806133a22873dd434092d827e52
Mirko Risuleo <mirkotm705@gmail.com>
2024-11-05 09:34:00 +0100

    Commit con lezione su database

--- src/main/java/org/generation/italy/esempiCorso/sql/prime/PrimaLettura.java
+++ src/main/java/org/generation/italy/esempiCorso/sql/prime/PrimaLettura.java
@@ -1,28 +1,28 @@
-package org.generation.italy.esempiCorso.sql.prime;
-
-import org.generation.italy.esempiCorso.sql.DatabaseConnection;
-
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-public class PrimaLettura {
-    public static void main(String[] args) {
-        try(Connection connection = DatabaseConnection.getConnection()) {
-            String query = "SELECT * FROM books";
-            Statement statement = connection.createStatement(); //Statement è un oggetto che trasporta informazioni al db, permettendoci di eseguire le query
-                                                                //sul nostro db
-            ResultSet righeLette = statement.executeQuery(query); //executeQuery() lo usiamo per le letture e gli passiamo una query
-            //l'executequery lo inseriamo dentro un ResultSet, che è ciò che l'execute mi restituisce
-            while(righeLette.next()) {
-                int id = righeLette.getInt("id");
-                String title = righeLette.getString("title");
-                System.out.println("Libro con id " + id + ", titolo: " + title);
-            }
-        }
-        catch (SQLException e){
-            e.printStackTrace();
-        }
-    }
-}
+//package org.generation.italy.esempiCorso.sql.prime;
+//
+//import org.generation.italy.esempiCorso.sql.DatabaseConnection;
+//
+//import java.sql.Connection;
+//import java.sql.ResultSet;
+//import java.sql.SQLException;
+//import java.sql.Statement;
+//
+//public class PrimaLettura {
+//    public static void main(String[] args) {
+//        try(Connection connection = DatabaseConnection.getConnection()) {
+//            String query = "SELECT * FROM books";
+//            Statement statement = connection.createStatement(); //Statement è un oggetto che trasporta informazioni al db, permettendoci di eseguire le query
+//                                                                //sul nostro db
+//            ResultSet righeLette = statement.executeQuery(query); //executeQuery() lo usiamo per le letture e gli passiamo una query
+//            //l'executequery lo inseriamo dentro un ResultSet, che è ciò che l'execute mi restituisce
+//            while(righeLette.next()) {
+//                int id = righeLette.getInt("id");
+//                String title = righeLette.getString("title");
+//                System.out.println("Libro con id " + id + ", titolo: " + title);
+//            }
+//        }
+//        catch (SQLException e){
+//            e.printStackTrace();
+//        }
+//    }
+//}
--- src/main/java/org/generation/italy/esempiCorso/sql/prime/QueryParametrizzataBella.java
+++ src/main/java/org/generation/italy/esempiCorso/sql/prime/QueryParametrizzataBella.java
@@ -1,36 +1,36 @@
-package org.generation.italy.esempiCorso.sql.prime;
-
-import org.generation.italy.esempiCorso.sql.DatabaseConnection;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-public class QueryParametrizzataBella {
-
-    public static void main(String[] args) {
-        String mioTitolo = "Roma";
-        try(Connection connection = DatabaseConnection.getConnection()) {
-            String query = "SELECT * FROM books WHERE titolo = ? OR titolo = ?";
-            PreparedStatement ps = connection.prepareStatement(query);
-            ps.setString(1, "Narnia"); //serve per cambiare il ? in stringa.
-            // come parametri prende un int(Che indica il punto interrogativo in che posizione sta (prende in considerazione solo i punti interrogativi)
-            //poi dopo la virgola, la stringa che si vuole sostituire
-            //Al posto della stringa posso usare anche una variabile
-            ps.setString(2, mioTitolo);
-            ResultSet rs = ps.executeQuery(); //non va inserita la query perchè l'abbiamo già passata in riga 12
-            while (rs.next()) {
-                int id = rs.getInt("id");
-                String title = rs.getString("titolo");
-                System.out.println("Libro con id " + id + ", titolo: " + title);
-            }
-
-        }
-        catch(SQLException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-}
+//package org.generation.italy.esempiCorso.sql.prime;
+//
+//import org.generation.italy.esempiCorso.sql.DatabaseConnection;
+//
+//import java.sql.Connection;
+//import java.sql.PreparedStatement;
+//import java.sql.ResultSet;
+//import java.sql.SQLException;
+//
+//public class QueryParametrizzataBella {
+//
+//    public static void main(String[] args) {
+//        String mioTitolo = "Roma";
+//        try(Connection connection = DatabaseConnection.getConnection()) {
+//            String query = "SELECT * FROM books WHERE titolo = ? OR titolo = ?";
+//            PreparedStatement ps = connection.prepareStatement(query);
+//            ps.setString(1, "Narnia"); //serve per cambiare il ? in stringa.
+//            // come parametri prende un int(Che indica il punto interrogativo in che posizione sta (prende in considerazione solo i punti interrogativi)
+//            //poi dopo la virgola, la stringa che si vuole sostituire
+//            //Al posto della stringa posso usare anche una variabile
+//            ps.setString(2, mioTitolo);
+//            ResultSet rs = ps.executeQuery(); //non va inserita la query perchè l'abbiamo già passata in riga 12
+//            while (rs.next()) {
+//                int id = rs.getInt("id");
+//                String title = rs.getString("titolo");
+//                System.out.println("Libro con id " + id + ", titolo: " + title);
+//            }
+//
+//        }
+//        catch(SQLException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+//
+//}
