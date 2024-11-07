c1786ed63a3cc1f2f56ae0b62b29a17b0c64774f
Samuele Celletti <samuele03@hotmail.it>
2024-11-07 08:55:37 +0100

    Riordine file

--- src/main/java/org/generation/italy/esempiCorso/eserciziMiei/aeroporto/daos/DaoException.java
+++
@@ -1,7 +0,0 @@
-package org.generation.italy.esempiCorso.eserciziMiei.aeroporto.daos;
-
-public class DaoException extends Exception {
-    public DaoException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
