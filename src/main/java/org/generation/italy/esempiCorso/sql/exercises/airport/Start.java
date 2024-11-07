6b83e6fb5d2c21cdbe56ee34b72b8ae43c27af98
Marco Falconetti <falconetti.grafico@gmail.com>
2024-11-07 12:36:21 +0100

    Avanzando con l'esercizio di Hibernate, mergiato la master sulla mia brench e apportate modifiche alla main.

---
+++ src/main/java/org/generation/italy/esempiCorso/sql/exercises/airport/Start.java
@@ -0,0 +1,28 @@
+package org.generation.italy.esempiCorso.sql.exercises.airport;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.generation.italy.esempiCorso.sql.exercises.airport.model.Airport;
+import org.generation.italy.esempiCorso.sql.exercises.airport.model.Passenger;
+
+
+public class Start {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hibernatefundamentals");
+        EntityManager em = emf.createEntityManager();
+//        Airport a1 = new Airport(0, "Malpensa");
+//        em.getTransaction().begin();
+//        em.persist(a1);
+//        em.getTransaction().commit();
+
+        em.getTransaction().begin();
+        Airport found = em.find(Airport.class, 1);
+        if(found != null) {
+            System.out.println(found.getName());
+        }
+        Airport other = new Airport(0,"Linate");
+        Class<Airport> x = Airport.class; // Oggetto di tipo (class) che descrive la struttura della classe.
+        Class<Airport> y = (Class<Airport>) found.getClass(); // La stessa cosa di sopra solo piu specifico.
+    }
+}
