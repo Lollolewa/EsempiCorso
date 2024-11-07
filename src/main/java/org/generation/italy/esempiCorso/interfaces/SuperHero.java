23086913849921efa063d49a43cb85b9b6a3b061
Mirko Risuleo <mirkotm705@gmail.com>
2024-11-07 13:05:35 +0100

    Commit post lezione hibernate e transazioni su db con hibernate

--- src/main/java/org/generation/italy/esempiCorso/interfaces/SuperHero.java
+++ src/main/java/org/generation/italy/esempiCorso/interfaces/SuperHero.java
@@ -1,19 +1,5 @@
 package org.generation.italy.esempiCorso.interfaces;
 
-public class SuperHero extends FlyObject implements HumanBeing{
-    //si scrive prima l'extends, poi gli implements
-    @Override
-    public void speak() {
-
-    }
-
-    @Override
-    public void eat() {
-
-    }
-
-    @Override
-    public void work() {
-
-    }
+public class SuperHero {
+    int id;
 }
