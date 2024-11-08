5d2c7269c66b83c25dbf4d928f2fa3df0ec9ed6f
Joshua Castellani <joshuacastellani@gmail.com>
2024-11-08 09:41:05 +0100

    AirportDaoJpa completo

--- src/main/java/org/generation/italy/esempiCorso/sql/aereoporto/daos/AirportDaoJpa.java
+++ src/main/java/org/generation/italy/esempiCorso/sql/aereoporto/daos/AirportDaoJpa.java
@@ -1,12 +1,13 @@
 package org.generation.italy.esempiCorso.sql.aereoporto.daos;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceException;
 import org.generation.italy.esempiCorso.sql.aereoporto.entities.Airport;
 
 import java.util.List;
 import java.util.Optional;
 
-public class AirportDaoJpa implements AirportDao{
+public class AirportDaoJpa implements AirportDao {
     private EntityManager em;
 
     public AirportDaoJpa(EntityManager em) {
@@ -16,27 +17,75 @@ public class AirportDaoJpa implements AirportDao{
     @Override
     public Airport create(Airport toSave) throws DaoException {
         em.getTransaction().begin();
-        em.persist(toSave);
-        return null;
+        try {
+            em.persist(toSave);
+            return toSave;
+        } catch (PersistenceException e) {
+            em.getTransaction().rollback();
+            throw new DaoException(e.getMessage(), e);
+        }
     }
 
     @Override
     public Optional<Airport> findById(int id) throws DaoException {
-        return Optional.empty();
+        em.getTransaction().begin();
+        try {
+            Airport a = em.find(Airport.class, id);
+            em.getTransaction().commit();
+            return Optional.ofNullable(a);
+        } catch (PersistenceException e) {
+            em.getTransaction().rollback();
+            throw new DaoException(e.getMessage(), e);
+        }
     }
+
 
     @Override
     public boolean update(Airport newAirport) throws DaoException {
-        return false;
+        em.getTransaction().begin();
+        try {
+            Airport found = em.find(Airport.class, newAirport.getId());
+            if (found == null) {
+                em.getTransaction().rollback();
+                return false;
+            }
+            em.merge(newAirport);
+            em.getTransaction().commit();
+            return true;
+        } catch (PersistenceException e) {
+            em.getTransaction().rollback();
+            throw new DaoException(e.getMessage(), e);
+        }
     }
 
     @Override
     public boolean delete(int airportID) throws DaoException {
-        return false;
+        em.getTransaction().begin();
+        try {
+            Airport found = em.find(Airport.class, airportID);
+            if (found == null) {
+                em.getTransaction().rollback();
+                return false;
+            }
+            em.remove(found);
+            em.getTransaction().commit();
+            return true;
+        } catch (PersistenceException e) {
+            em.getTransaction().rollback();
+            throw new DaoException(e.getMessage(), e);
+        }
     }
 
     @Override
     public List<Airport> findAll() throws DaoException {
-        return List.of();
+        em.getTransaction().begin();
+        try {
+            return em.createQuery("SELECT FROM aeroporto a", Airport.class).getResultList();
+        } catch (PersistenceException e) {
+            em.getTransaction().rollback();
+            throw new DaoException(e.getMessage(), e);
+        }
     }
+
 }
+
