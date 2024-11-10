3389cdc7646e97855ff24035a235201902c26ca0
GabrieleSBT <gabrielesb@outlook.it>
2024-11-10 18:02:56 +0100

    test git

--- src/main/java/org/generation/italy/examples/interfaces/travelagencymio/Start.java
+++ src/main/java/org/generation/italy/examples/interfaces/travelagencymio/Start.java
@@ -1,11 +1,23 @@
 package org.generation.italy.examples.interfaces.travelagencymio;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Start {
     public static void main(String[] args) {
 
         UserInterface ui = new UserInterface();
 
         ui.begin();
+
+        List<String> lista = new ArrayList<>();
+        findDispari(lista).
+        lista.forEach(a -> {
+            a.toUpperCase()
+        });
+
+        private List<String> findDispari(List lista){
+
     }
 }
 
